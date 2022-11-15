ce2eb185d53e9efca6b7ac8314d5cdde73b9fe81
Kyle Parisher <kyle.parisher@owasp.org>
2022-11-15 02:47:38 -0400

    Create zerotier-net-conf.sh

---
+++ zerotier-net-conf.sh
@@ -0,0 +1,31 @@
+cat << EOF | sudo tee /etc/systemd/network/25-bridge-br0.network
+[Match]
+Name=$BR_IF
+
+[Network]
+Address=$BR_ADDR
+Gateway=$GW_ADDR
+DNS=1.1.1.1
+EOF
+
+cat << EOF | sudo tee /etc/systemd/network/br0.netdev
+[NetDev]
+Name=$BR_IF
+Kind=bridge
+EOF
+
+cat << EOF | sudo tee /etc/systemd/network/25-bridge-br0-zt.network
+[Match]
+Name=$ZT_IF
+
+[Network]
+Bridge=$BR_IF
+EOF
+
+cat << EOF | sudo tee /etc/systemd/network/25-bridge-br0-en.network   
+[Match]
+Name=eth0 # might be en*
+
+[Network]
+Bridge=$BR_IF
+EOF
